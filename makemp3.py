fb9cb95d8fc50d8a746e08201c2d7513e3074933
wenbinlu <windbinderue@gmail.com>
2023-11-22 15:58:31 -0800

    updated all files

--- makemp3.py
+++ makemp3.py
@@ -1,5 +1,4 @@
 from openai import OpenAI
-from playsound import playsound
 import time
 from datetime import datetime
 from glob import glob
@@ -13,47 +12,37 @@ all_files = glob(fdir + today + '/*.txt')
 id_list = [f.replace(fdir + today + '/', '').replace('.txt', '') for f in all_files]
 
 print(id_list)
+for id in id_list:
+    abstr_fname = id + '.txt'
+    audio_savname = id + '.mp3'
+    cont_command = 'Provide a short summary for the following abstract of a recent paper,' \
+                   'including the main methods and findings. ' \
+                   'Please include the title.'\
+                   + 'Keep in mind that your audience is an astrophysicist expert.'
 
-exit()
-abstr_fname = arxiv_id + '.txt'
-audio_savname = arxiv_id + '.mp3'
-cont_command = 'Provide a short summary for the following abstract of a recent paper,' \
-               'including the main methods and findings. ' \
-               'Please include the title.'\
-               + 'Keep in mind that your audience is an astrophysicist expert.'
+    # read the abstract
+    with open(fdir + today + '/' + abstr_fname, 'r') as f:
+        abstr = f.readline()
 
-# read the abstract
-with open(fdir + abstr_fname, 'r') as f:
-    abstr = f.readline()
+    # create a digest
+    completion = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system", "content": "You are a professional astrophysicist."},
+        {"role": "user", "content": cont_command},
+        {"role": "user", "content": abstr}
+      ]
+    )
 
-# create a digest
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a professional astrophysicist."},
-    {"role": "user", "content": cont_command},
-    {"role": "user", "content": abstr}
-  ]
-)
+    response_txt = completion.choices[0].message.content
+    print('---The generated text:', response_txt)
 
-response_txt = completion.choices[0].message.content
-print('---The generated text:', response_txt)
+    # convert txt to audio
+    response_audio = client.audio.speech.create(
+      model="tts-1",
+      voice="alloy",
+      input=response_txt,
+    )
 
-# convert txt to audio
-response_audio = client.audio.speech.create(
-  model="tts-1",
-  voice="alloy",
-  input=response_txt,
-)
-
-# save the audio to a file
-response_audio.stream_to_file(fdir + audio_savname)
-
-mid_time = time.time()
-print("---It took %s seconds to make the .mp3 file" % (mid_time - start_time))
-
-# play the mp3 file
-playsound(fdir + audio_savname)
-
-end_time = time.time()
-print("---It took %s seconds to play the audio" % (end_time - mid_time))
+    # save the audio to a file
+    response_audio.stream_to_file(fdir + today + '/' + audio_savname)
