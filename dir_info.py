9351a5cd323d7836305764554a079b5c3cfc2c93
wenbinlu <windbinderue@gmail.com>
2023-11-24 11:42:14 -0800

    "today" is according to datetime

--- dir_info.py
+++ dir_info.py
@@ -3,5 +3,5 @@ from datetime import datetime
 codedir = '/Users/wenbinlu/Dropbox/readarXiv/'
 datadir = codedir + 'arXivmp3_data/'
 
-# today = datetime.today().strftime('%Y-%m-%d')
-today = '2023-11-23'
+today = datetime.today().strftime('%Y-%m-%d')
+# today = '2023-11-23'
